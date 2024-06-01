c05bc9cd08f54667b5bf7ae71276755860efc00b
u-khang <khang.nguyen8675@gmail.com>
2024-06-01 14:24:16 -0400

    template

---
+++ src/components/Home.jsx
@@ -0,0 +1,42 @@
+import { Link } from "react-router-dom";
+import "../styles/Home.css";
+import homepageExtraLarge from "../images/homepage-extra-large.jpg";
+import homepageLarge from "../images/homepage-large.jpg";
+import homepageMedium from "../images/homepage-medium.jpg";
+import homepageSmall from "../images/homepage-small.jpg";
+import ProductItem from "./ProductItem";
+
+function Home({ featured = [] }) {
+    return (
+      <div className="home">
+        <section className="top-section">
+          <img
+            src={homepageLarge}
+            srcSet={`${homepageExtraLarge} 6912w, ${homepageLarge} 2400w, ${homepageMedium} 1400w, ${homepageSmall} 480w`}
+            alt="living room with furniture"
+            className="homepage-img"
+          />
+          <div className="image-text-overlay">
+            <h1 className="image-text">Make room for better living</h1>
+            <Link to="/shop" className="shop-now">
+              Shop Now
+            </Link>
+          </div>
+        </section>
+        <section className="bottom-section">
+          <div className="featured-items-container">
+            <h1 className="featured-items-heading">Featured Items</h1>
+            <ul className="featured-items">
+              {featured.map((product) => (
+                <li key={product.id}>
+                  <ProductItem product={product} />
+                </li>
+              ))}
+            </ul>
+          </div>
+        </section>
+      </div>
+    );
+  }
+  
+  export default Home;
